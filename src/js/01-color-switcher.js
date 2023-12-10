6252f11bdf3e5ffc6b0b23422edfacfccb8c07ef
AndriiPyndyk <andronnn@gmail.com>
2023-12-10 10:47:43 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+const DELAY = 1000;
 
+let intervalId = null;
+
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
